6f5b36155dbec3a750b22f7387a1cd3fd0322d5d
HWANGJUNG TEK <dingaking@gmail.com>
2016-08-14 12:35:46 +0900

    refactoring complete.

--- src/main.go
+++ src/main.go
@@ -1,15 +1,21 @@
+////////////////////////////////////////////////////////////////////////
+// OK, We Need to Split This Up!
+// At this point, the project needs a little refactoring. We have too much going on in just a few files.
+// We are going to now create the following files and move the code around accordingly:
+// - main.go
+// - hadlers.go
+// - routes.go
+// - todo.go
+////////////////////////////////////////////////////////////////////////
 package main
 
 import (
-	"fmt"
-	"html"
 	"log"
 	"net/http"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
-	})
-	log.Fatal(http.ListenAndServe(":8081", nil))
+
+	router := NewRouter()
+	log.Fatal(http.ListenAndServe(":8082", router))
 }
