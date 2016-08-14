d54b09cddeb871b2b8317ea0cec5c02dbe9e158b
HWANGJUNG TEK <dingaking@gmail.com>
2016-08-14 17:34:31 +0900

    logging

--- src/logger.go
+++ src/logger.go
@@ -1,11 +1,14 @@
 ////////////////////////////////////////////////////////////////////////
 // Outputting a Web Log
-// In splitting up the routes file,
+// In splitting up the routes file, I also had an ulterio motive. As you will see shortly, it now becomes very easy to decorate my http handlers with additional functonality.
+// Let's start with the ability to log out web requests like most modern web services do. In Go, there is no web logging package or functionality in the standard library, so we have to create it.
+// We'll do that by creating a file called logger.go and add the following code
 ////////////////////////////////////////////////////////////////////////
 
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"time"
@@ -15,8 +18,11 @@ func Logger(inner http.Handler, name string) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		start := time.Now()
 
+		fmt.Println("ddddddd")
+
 		inner.ServeHTTP(w, r)
 
+		fmt.Printf("dddd")
 		log.Printf(
 			"%\t%s\t%s\t%s",
 			r.Method,
@@ -26,3 +32,8 @@ func Logger(inner http.Handler, name string) http.Handler {
 		)
 	})
 }
+
+////////////////////////////////////////////////////////////////////////
+// This is a very standard idiom in Go. Effectively we are going to pass our handler to this function, which wil then wrap the passed handler with logging and timing functionality.
+// Next, we will need to utilize the logger decorator in our routes.
+////////////////////////////////////////////////////////////////////////
