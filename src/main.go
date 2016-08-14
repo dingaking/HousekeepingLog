0497f546718d0a2e913ecf5d10a54e173dc2636b
HWANGJUNG TEK <dingaking@gmail.com>
2016-08-14 09:09:21 +0900

    default http

--- src/main.go
+++ src/main.go
@@ -1,8 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"html"
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("dkdkdk")
-	fmt.Println("dkdkdkdkdkdkddk")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+	})
+	log.Fatal(http.ListenAndServe(":8081", nil))
 }
