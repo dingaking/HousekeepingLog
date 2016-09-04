80599d22d32be2b66813456c976e77a0b4a92e99
HWANGJUNG TEK <dingaking@gmail.com>
2016-09-05 08:33:12 +0900

    object에서 값 가져오는 부분 구현중.

--- src/apis/admin/source/components/LoginForm.react.js
+++ src/apis/admin/source/components/LoginForm.react.js
@@ -1,17 +1,36 @@
 var React = require('react');
 var Axios = require('axios');
 
-
 var LoginForm = React.createClass({
+
+	getInitialState: function () {
+		return {
+			userid: '',
+			password: ''
+		};
+	},
+
+	useridChange: function (e) {
+		console.log(e.target.value);
+		this.setState({
+			userid: e.target.value
+		})
+	},
+
+	passwordChange: function (e) {
+		this.setState({
+			password: e.target.value
+		})
+	},
 
 	requestLogin : function (e) {
 		e.preventDefault(); 
-		
-		console.log('requestlogin');
-
-		Axios.post('/api/authC').then(response => {
-			console.log(response);
-            //this.props.onReceive(response.data.number);
+		console.log(this.state.userid);
+		Axios.post('/api/authC', {
+			userid: this.state.userid,
+			password: this.state.password
+		}).then(response => {
+			console.log(response.request.response);
         });
 	},
 
@@ -20,9 +39,9 @@ var LoginForm = React.createClass({
 			<div className="loginmodal-container">
 				<h1>Login to Your Account</h1><br />
 				<form>
-					<input type="text" name="user" placeholder="Username" />
-					<input type="password" name="pass" placeholder="Password" />
-					<input type="submit" name="login" className="login loginmodal-submit" value="Login" onClick={this.requestLogin} />
+					<input type="text" name="user" placeholder="Username" valChange={this.useridChange} val={this.state.userid} />
+					<input type="password" name="pass" placeholder="Password" valChange={this.passwordChange} />
+					<input type="submit" name="login" className="login loginmodal-submit" value="Login" onClick={this.requestLogin} val={this.state.password} />
 				</form>
 					
 				<div className="login-help">
