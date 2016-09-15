9508f0221b5e2b5d3340e2dd2fe793aca3b17206
HWANGJUNG TEK <dingaking@gmail.com>
2016-09-15 12:18:28 +0900

    가계부 서비스 회원정보 조회(사용자)

--- src/apis/apidoc/body.js
+++ src/apis/apidoc/body.js
@@ -15,8 +15,36 @@
  *
  * @apiExample {curl} Example usage:
  * curl -X POST -H "Accept: Application/json" -H "Content-Type: application/json" \
--d '{"userid":"admin", "password":"12345678"}' \
+-d '{"userid":"admin@localhost.com", "password":"12345678"}' \
 http://localhost:8082/api/authC
+ *
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *         "result": "success",
+ *         "err_msg":""
+ *     }
+ *
+ */
+
+/**
+ * @api {post} /api/authR authR
+ * @apiVersion 0.1.0
+ * @apiName AuthRead
+ * @apiDescription 가계부 서비스 회원정보 조회
+ * @apiGroup Auth
+ *
+ * @apiParam {String} userid userid(email)
+ * @apiParam {String} password 비밀번호(8~16자리)
+ *
+ * @apiSuccess {String} result 가입요청 결과 "success" or "fail"
+ * @apiSuccess {String} err_msg result가 "fail"인 경우 에러 메시지가 "success"인 경우 ""
+ * @apiSuccess {String} access_token result가 "success"인 경우
+ *
+ * @apiExample {curl} Example usage:
+ * curl -X POST -H "Accept: Application/json" -H "Content-Type: application/json" \
+-d '{"userid":"admin@localhost.com", "password":"12345678"}' \
+http://localhost:8082/api/authR
  *
  * @apiSuccessExample Success-Response:
  *     HTTP/1.1 200 OK
