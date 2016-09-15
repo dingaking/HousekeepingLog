cae626fed946cf3481627fd7987a6b46fb754a14
HWANGJUNG TEK <dingaking@gmail.com>
2016-09-15 12:18:05 +0900

    관리자 api 회원목록 조회 api 추가.

--- src/apis/apidoc/body.js
+++ src/apis/apidoc/body.js
@@ -1,7 +1,7 @@
 // body.json
 
 /**
- * @api {post} /api/authC AuthC
+ * @api {post} /api/authC authC
  * @apiVersion 0.1.0
  * @apiName AuthCreate
  * @apiDescription 가계부 서비스 회원가입
@@ -32,7 +32,7 @@ http://localhost:8082/api/authC
  * @apiVersion 0.1.0
  * @apiName AdminUserR
  * @apiDescription 가계부 관리자 로그인
- * @apiGroup AdminUser
+ * @apiGroup Admin_User
  *
  * @apiParam {String} userid userid
  * @apiParam {String} password 비밀번호
@@ -60,7 +60,7 @@ http://localhost:8082/api/admin/userR
  * @apiVersion 0.1.0
  * @apiName AdminUserU
  * @apiDescription 가계부 관리자 정보 수정
- * @apiGroup AdminUser
+ * @apiGroup Admin_User
  *
  * @apiParam (AdminUserU_Action_1) {String} action 1:admin init password change
  * @apiParam (AdminUserU_Action_1) {String} userid userid
@@ -95,4 +95,54 @@ http://localhost:8082/api/admin/userU
 /**
  * @apiDefine AdminUserU_Action_1
  * Parameter(SubAction 1)
+ */
+
+
+/**
+ * @api {post} /api/admin/userL userL
+ * @apiVersion 0.1.0
+ * @apiName AdminUserL
+ * @apiDescription 가계부 관리자 회원목록
+ * @apiGroup Admin_User
+ *
+ * @apiParam (AdminUserL_Action_1) {String} action 1:회원목록 조회
+ * @apiParam (AdminUserL_Action_1) {String} access_token 인증키
+ * @apiParam (AdminUserL_Action_1) {String} page 목록 페이지
+ *
+ * @apiParam (AdminUserL_Action_2) {String} action s:회원목록 검색
+ * @apiParam (AdminUserL_Action_2) {String} access_token 인증키
+ * @apiParam (AdminUserL_Action_2) {String} search_id 검색어(userid)
+ * @apiParam (AdminUserL_Action_2) {String} search_name 검색어(username)
+ * @apiParam (AdminUserL_Action_2) {String} page 검색 결과 페이지
+ *
+ * @apiSuccess {String} result "success" or "fail"
+ * @apiSuccess {String} err_msg result가 "fail"인 경우
+ * @apiSuccess {String} data result가 "success"인 경우
+ *
+ * @apiExample {curl} Example usage:
+ * curl -X POST  -H "Accept: Application/json" -H "Content-Type: application/json" \
+-d '{"action":"1", "access_token":"access_token_value"}' \
+http://localhost:8082/api/admin/userL
+ * 
+ * @apiSuccessExample Success-Response[SubAction:1]:
+ * HTTP/1.1 200 OK
+ * {
+ *     "result":"success",
+ *     "access_token":"111111"
+ * }
+ * 
+ * @apiSuccessExample Success-Response[SubAction:2]:
+ * HTTP/1.1 200 OK
+ * {
+ *     "result":"success",
+ *     "access_token":"111111"
+ * }
+ * 
+ * 
+ */
+/**
+ * @apiDefine AdminUserL_Action_1 Parameter(SubAction 1)
+ */
+/**
+ * @apiDefine AdminUserL_Action_2 Parameter(SubAction 2)
  */
