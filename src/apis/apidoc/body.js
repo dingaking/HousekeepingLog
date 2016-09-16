50f374e9afef661e78ba4f2bb89794af88089471
HWANGJUNG TEK <dingaking@gmail.com>
2016-09-16 11:10:11 +0900

    사용자 API 문서 : 단말기.

--- src/apis/apidoc/body.js
+++ src/apis/apidoc/body.js
@@ -13,7 +13,7 @@
  * @apiSuccess {String} result 요청 결과 "success" or "fail"
  * @apiSuccess {String} err_msg result가 "fail"인 경우 에러 메시지
  *
- * @apiExample {curl} Example usage:
+ * @apiExample {curl} Example :
  * curl -X POST -H "Accept: Application/json" -H "Content-Type: application/json" \
 -d '{"userid":"admin@localhost.com", "password":"12345678"}' \
 http://localhost:8082/api/authC
@@ -99,6 +99,7 @@ http://localhost:8082/api/authU
  * @apiParam (Param_Action_1) {String} userid userid(email)
  * @apiParam (Param_Action_1) {String} old_password 이전 비밀번호
  * @apiParam (Param_Action_1) {String} new_password 새 비밀번호
+ * @apiParam (Param_Action_1) {String} access_token 인증키
  *
  * @apiParam (Param_Action_2) {String} action 2 : 회원정보 변경
  * @apiParam (Param_Action_2) {String} dispname 대화명
@@ -106,6 +107,7 @@ http://localhost:8082/api/authU
  * @apiParam (Param_Action_2) {String} profile 프로필 사진
  * @apiParam (Param_Action_2) {String} phone_number 전화번호
  * @apiParam (Param_Action_2) {String} public 그룹에 공개여부
+ * @apiParam (Param_Action_2) {String} access_token 인증키
  *
  * @apiSuccess {String} result "success" or "fail"
  * @apiSuccess {String} err_msg result가 "fail"인 경우 에러 메시지
@@ -142,11 +144,163 @@ http://localhost:8082/api/authU
  * @apiSuccess {String} result 요청 결과 "success" or "fail"
  * @apiSuccess {String} err_msg result가 "fail"인 경우 에러 메시지
  *
- * @apiExample {curl} Example usage:
+ * @apiExample {curl} Example :
  * curl -X POST -H "Accept: Application/json" -H "Content-Type: application/json" \
 -d '{"userid":"admin@localhost.com", "password":"12345678", \
 "access_token":"ef53163004dd7257c52e9571fff5751f72940bdd"}' \
 http://localhost:8082/api/authD
+ *
+ */
+
+/**
+ * @api {post} /api/deviceC deviceC
+ * @apiVersion 0.1.0
+ * @apiName DeviceCreate
+ * @apiDescription 단말기 추가
+ * @apiGroup Device
+ *
+ * @apiExample {curl} Example :
+ * curl -X POST -H "Accept: Application/json" -H "Content-Type: application/json" \
+-d '{"terminal_id":"dk8s88a0ffd8a", "device_type":"i", \
+"access_token":"ef53163004dd7257c52e9571fff5751f72940bdd"}' \
+http://localhost:8082/api/deviceC
+ *
+ * @apiParam {String} access_token 인증키
+ * @apiParam {String} terminal_id UDID
+ * @apiParam {String} device_type i:iOS, a:Android, w:Web
+ *
+ * @apiSuccess {String} result 요청 결과 "success" or "fail"
+ * @apiSuccess {String} err_msg result가 "fail"인 경우 에러 메시지
+ * @apiSuccess {String} terminal_no 터미널no
+ *
+ */
+
+/**
+ * @api {post} /api/deviceR deviceR
+ * @apiVersion 0.1.0
+ * @apiName DeviceRead
+ * @apiDescription 단말기 정보 조회
+ * @apiGroup Device
+ *
+ * @apiExample {curl} Example :
+ * curl -X POST -H "Accept: Application/json" -H "Content-Type: application/json" \
+-d '{"terminal_no":"34", \
+"access_token":"ef53163004dd7257c52e9571fff5751f72940bdd"}' \
+http://localhost:8082/api/deviceR
+ *
+ * @apiParam {String} access_token 인증키
+ * @apiParam {String} terminal_no 터미널no
+ *
+ * @apiSuccess {String} result 요청 결과 "success" or "fail"
+ * @apiSuccess {String} err_msg result가 "fail"인 경우 에러 메시지
+ * @apiSuccess {String} terminal_id UDID(web null)
+ * @apiSuccess {String} device_type i:iOS, a:Android, w:Web
+ * @apiSuccess {String} device_token push키
+ * @apiSuccess {String} create_datetime 등록일시
+ * @apiSuccess {String} notification 1:ON, 0:OFF
+ * @apiSuccess {String} state 1:ON(사용),0:OFF(삭제)
+ * @apiSuccess {String} terminal_name 단말기 별칭
+ * @apiSuccess {String} last_access 최근접속 일시
+ */
+
+/**
+ * @api {post} /api/deviceU deviceU
+ * @apiVersion 0.1.0
+ * @apiName DeviceUpdate
+ * @apiDescription 단말기 정보 갱신
+ * @apiGroup Device
+ *
+ * @apiExample {curl} Example :
+ * curl -X POST -H "Accept: Application/json" -H "Content-Type: application/json" \
+-d '{"access_token":"ef53163004dd7257c52e9571fff5751f72940bdd", \
+"terminal_no":"36", "terminal_name":"my iphone"}' \
+http://localhost:8082/api/deviceU
+ *
+ * @apiParam (Param_Action_1) {String} access_token 인증키
+ * @apiParam (Param_Action_1) {String} terminal_no 터미널no
+ * @apiParam (Param_Action_1) {String} [device_token] push키
+ * @apiParam (Param_Action_1) {String} [terminal_name] 단말기 별칭
+ * @apiParam (Param_Action_1) {String} [notification] 1:ON, 0:OFF
+ *
+ * @apiSuccess {String} result "success" or "fail"
+ * @apiSuccess {String} err_msg result가 "fail"인 경우 에러 메시지
+ */
+
+/**
+ * @api {post} /api/deviceD deviceD
+ * @apiVersion 0.1.0
+ * @apiName DeviceDelete
+ * @apiDescription 단말기 삭제
+ * @apiGroup Device
+ *
+ * @apiExample {curl} Example :
+ * curl -X POST -H "Accept: Application/json" -H "Content-Type: application/json" \
+-d '{"access_token":"ef53163004dd7257c52e9571fff5751f72940bdd", \
+"terminal_no":"36"}' \
+http://localhost:8082/api/deviceD
+ *
+ * @apiParam (Param_Action_1) {String} access_token 인증키
+ * @apiParam (Param_Action_1) {String} terminal_no 터미널no
+ *
+ * @apiSuccess {String} result "success" or "fail"
+ * @apiSuccess {String} err_msg result가 "fail"인 경우 에러 메시지
+ *
+ */
+
+/**
+ * @api {post} /api/deviceL deviceL
+ * @apiVersion 0.1.0
+ * @apiName DeviceList
+ * @apiDescription 단말기 목록
+ * @apiGroup Device
+ *
+ * @apiExample {curl} Example :
+ * curl -X POST -H "Accept: Application/json" -H "Content-Type: application/json" \
+-d '{"access_token":"ef53163004dd7257c52e9571fff5751f72940bdd"}' \
+http://localhost:8082/api/deviceL
+ *
+ * @apiParam {String} access_token 인증키
+ *
+ * @apiSuccess {String} result "success" or "fail"
+ * @apiSuccess {String} err_msg result가 "fail"인 경우 에러 메시지
+ * @apiSuccess {Object[]} data 단말기 목록
+ * @apiSuccess {String} data.terminal_id UDID(web null)
+ * @apiSuccess {String} data.device_type i:iOS, a:Android, w:Web
+ * @apiSuccess {String} data.device_token push키
+ * @apiSuccess {String} data.create_datetime 등록일시
+ * @apiSuccess {String} data.notification 1:ON, 0:OFF
+ * @apiSuccess {String} data.state 1:ON(사용),0:OFF(삭제)
+ * @apiSuccess {String} data.terminal_name 단말기 별칭
+ * @apiSuccess {String} data.last_access 최근접속 일시
+ *
+ */
+
+/**
+ * @api {post} /api/deviceS deviceS
+ * @apiVersion 0.1.0
+ * @apiName DeviceSearch
+ * @apiDescription 단말기 검색
+ * @apiGroup Device
+ *
+ * @apiExample {curl} Example :
+ * curl -X POST -H "Accept: Application/json" -H "Content-Type: application/json" \
+-d '{"access_token":"ef53163004dd7257c52e9571fff5751f72940bdd", "terminal_name":"my"}' \
+http://localhost:8082/api/deviceS
+ *
+ * @apiParam {String} access_token 인증키
+ * @apiParam {String} terminal_name 검색어
+ *
+ * @apiSuccess {String} result "success" or "fail"
+ * @apiSuccess {String} err_msg result가 "fail"인 경우 에러 메시지
+ * @apiSuccess {Object[]} data 단말기 목록
+ * @apiSuccess {String} data.terminal_id UDID(web null)
+ * @apiSuccess {String} data.device_type i:iOS, a:Android, w:Web
+ * @apiSuccess {String} data.device_token push키
+ * @apiSuccess {String} data.create_datetime 등록일시
+ * @apiSuccess {String} data.notification 1:ON, 0:OFF
+ * @apiSuccess {String} data.state 1:ON(사용),0:OFF(삭제)
+ * @apiSuccess {String} data.terminal_name 단말기 별칭
+ * @apiSuccess {String} data.last_access 최근접속 일시
  *
  */
 
@@ -164,7 +318,7 @@ http://localhost:8082/api/authD
  * @apiSuccess {String} err_msg result가 "fail"인 경우
  * @apiSuccess {String} access_token result가 "success"인 경우
  *
- * @apiExample {curl} Example usage:
+ * @apiExample {curl} Example :
  * curl -X POST  -H "Accept: Application/json" -H "Content-Type: application/json" \
 -d '{"userid":"admin", "password":"admin"}' \
 http://localhost:8082/api/admin/userR
@@ -207,7 +361,7 @@ http://localhost:8082/api/admin/userU
  * @apiDescription 가계부 관리자 회원목록
  * @apiGroup Admin_User
  *
- * @apiExample {curl} Example usage:
+ * @apiExample {curl} Example :
  * curl -X POST  -H "Accept: Application/json" -H "Content-Type: application/json" \
 -d '{"action":"1", "access_token":"access_token_value"}' \
 http://localhost:8082/api/admin/userL
