32326cec3957d25e582da09db7e655b2ae6fff45
HWANGJUNG TEK <dingaking@gmail.com>
2016-09-17 11:28:00 +0900

    관리자 API 문서 : 회원관리

--- src/apis/apidoc/body.js
+++ src/apis/apidoc/body.js
@@ -122,12 +122,20 @@ http://localhost:8082/api/authU
  * Parameter(action=2)
  */
 /**
+ * @apiDefine Param_Action_3
+ * Parameter(action=3)
+ */
+/**
  * @apiDefine Response_Action_1
  * Response(action=1)
  */
 /**
  * @apiDefine Response_Action_2
  * Response(action=2)
+ */
+/**
+ * @apiDefine Response_Action_3
+ * Response(action=3)
  */
 
 /**
@@ -707,8 +715,8 @@ http://localhost:8082/api/itemS
  * @apiParam {String} [search.capitalno] 재산항목 no
  * @apiParam {String} [search.item_name] 항목이름
  * @apiParam {String} [search.item_value] 항목금액
- * @apiParam {String} [search.date_begin] 시작 일시
- * @apiParam {String} [search.date_end] 종료 일시
+ * @apiParam {String} [search.date_begin] 가입 시작 일시
+ * @apiParam {String} [search.date_end] 가입 종료 일시
  *
  * @apiSuccess {String} result "success" or "fail"
  * @apiSuccess {String} err_msg result가 "fail"인 경우 에러 메시지
@@ -840,7 +848,12 @@ http://localhost:8082/api/admin/userC
  * @apiDescription 가계부 사용자 조회(관리자용)
  * @apiGroup Admin_User
  *
- * @apiExample {curl} Example :
+ * @apiExample {curl} Example : (action=1)
+ * curl -X POST  -H "Accept: Application/json" -H "Content-Type: application/json" \
+-d '{"userid":"admin", "password":"admin"}' \
+http://localhost:8082/api/admin/userR
+ *
+ * @apiExample {curl} Example : (action=2)
  * curl -X POST  -H "Accept: Application/json" -H "Content-Type: application/json" \
 -d '{"userid":"admin", "password":"admin"}' \
 http://localhost:8082/api/admin/userR
@@ -849,9 +862,27 @@ http://localhost:8082/api/admin/userR
  * @apiParam (Param_Action_1) {String} userid userid
  * @apiParam (Param_Action_1) {String} password 비밀번호
  *
+ * @apiParam (Param_Action_2) {String} action 2 : 정보 조회
+ * @apiParam (Param_Action_2) {String} userno 사용자 no
+ * @apiParam (Param_Action_2) {String} access_token 인증키
+ *
  * @apiSuccess (Response_Action_1) {String} result "success" or "fail"
  * @apiSuccess (Response_Action_1) {String} err_msg result가 "fail"인 경우
  * @apiSuccess (Response_Action_1) {String} access_token result가 "success"인 경우
+ *
+ * @apiSuccess (Response_Action_2) {String} result "success" or "fail"
+ * @apiSuccess (Response_Action_2) {String} err_msg result가 "fail"인 경우
+ * @apiSuccess (Response_Action_2) {Object} data 회원 정보
+ * @apiSuccess (Response_Action_2) {String} data.userid login id(email)
+ * @apiSuccess (Response_Action_2) {String} data.usertype 5:사용자, 9:관리자
+ * @apiSuccess (Response_Action_2) {String} data.dispname 대화명
+ * @apiSuccess (Response_Action_2) {String} data.intro 자기소개
+ * @apiSuccess (Response_Action_2) {String} data.profile 프로필 사진
+ * @apiSuccess (Response_Action_2) {String} data.create_datetime 가입일시
+ * @apiSuccess (Response_Action_2) {String} data.phone_number 전화번호
+ * @apiSuccess (Response_Action_2) {String} data.state 1:ON(사용), 0:OFF(탈퇴)
+ * @apiSuccess (Response_Action_2) {String} data.activated 2:관리자 인증, 1:URL 인증,0:OFF 미인증
+ * @apiSuccess (Response_Action_2) {String} data.public 1 = 공개 0 = 비공개
  */
 
 /**
@@ -866,21 +897,65 @@ http://localhost:8082/api/admin/userR
 -d '{"action":"1", "userid":"admin", "old_password":"12345678", "new_password":"12345678"}' \
 http://localhost:8082/api/admin/userU
  *
+ * @apiExample {curl} Example : (action=2)
+ * curl -X POST  -H "Accept: Application/json" -H "Content-Type: application/json" \
+-d '{"action":"1", "userid":"admin", "old_password":"12345678", "new_password":"12345678"}' \
+http://localhost:8082/api/admin/userU
+ *
+ * @apiExample {curl} Example : (action=3)
+ * curl -X POST  -H "Accept: Application/json" -H "Content-Type: application/json" \
+-d '{"action":"1", "userid":"admin", "old_password":"12345678", "new_password":"12345678"}' \
+http://localhost:8082/api/admin/userU
+ *
  * @apiParam (Param_Action_1) {String} action 1 : admin init password change
  * @apiParam (Param_Action_1) {String} userid userid
  * @apiParam (Param_Action_1) {String} old_password 이전 비밀번호
  * @apiParam (Param_Action_1) {String} new_password 새 비밀번호
  *
- * @apiSuccess {String} result "success" or "fail"
- * @apiSuccess {String} err_msg result가 "fail"인 경우
- * @apiSuccess {String} access_token result가 "success"인 경우
- * 
- */
-/**
- * @apiDefine AdminUserU_Action_1
- * Parameter(SubAction 1)
+ * @apiParam (Param_Action_2) {String} action 2 : update user info
+ * @apiParam (Param_Action_2) {String} access_token 인증키
+ * @apiParam (Param_Action_2) {Object} data 갱신 대상 정보
+ * @apiParam (Param_Action_2) {String} [data.usertype] 5:사용자, 9:관리자
+ * @apiParam (Param_Action_2) {String} [data.dispname] 대화명
+ * @apiParam (Param_Action_2) {String} [data.intro] 자기소개
+ * @apiParam (Param_Action_2) {String} [data.phone_number] 전화번호
+ * @apiParam (Param_Action_2) {String} [data.state] 1:ON(사용), 0:OFF(탈퇴)
+ * @apiParam (Param_Action_2) {String} [data.activated] 2:관리자 인증, 1:URL 인증,0:OFF 미인증
+ * @apiParam (Param_Action_2) {String} [data.public] 1 = 공개 0 = 비공개
+ *
+ * @apiParam (Param_Action_3) {String} action 3 : update user profile
+ * @apiParam (Param_Action_3) {file} pimage 프로필 이미지
+ * @apiParam (Param_Action_3) {String} access_token 인증키
+ *
+ * @apiSuccess (Response_Action_1) {String} result "success" or "fail"
+ * @apiSuccess (Response_Action_1) {String} err_msg result가 "fail"인 경우
+ * @apiSuccess (Response_Action_1) {String} access_token result가 "success"인 경우
+ *
+ * @apiSuccess (Response(action=2,3)) {String} result "success" or "fail"
+ * @apiSuccess (Response(action=2,3)) {String} err_msg result가 "fail"인 경우
+ * @apiSuccess (Response(action=2,3)) {String} access_token result가 "success"인 경우
  */
 
+/**
+ * @api {post} /api/admin/userD userD
+ * @apiVersion 0.1.0
+ * @apiName UserDelete
+ * @apiDescription 가계부 관리자 회원 삭제
+ * @apiGroup Admin_User
+ *
+ * @apiExample {curl} Example :
+ * curl -X POST -H "Accept: Application/json" -H "Content-Type: application/json" \
+-d '{"access_token":"ef53163004dd7257c52e9571fff5751f72940bdd", \
+"userno":"36"}' \
+http://localhost:8082/api/admin/userD
+ *
+ * @apiParam {String} access_token 인증키
+ * @apiParam {String} userno 사용자 no
+ *
+ * @apiSuccess {String} result "success" or "fail"
+ * @apiSuccess {String} err_msg result가 "fail"인 경우 에러 메시지
+ *
+ */
 
 /**
  * @api {post} /api/admin/userL userL
@@ -894,24 +969,62 @@ http://localhost:8082/api/admin/userU
 -d '{"action":"1", "access_token":"access_token_value"}' \
 http://localhost:8082/api/admin/userL
  *
- * @apiParam (AdminUserL_Action_1) {String} action 1:회원목록 조회
- * @apiParam (AdminUserL_Action_1) {String} access_token 인증키
- * @apiParam (AdminUserL_Action_1) {String} page 목록 페이지
- *
- * @apiParam (AdminUserL_Action_2) {String} action s:회원목록 검색
- * @apiParam (AdminUserL_Action_2) {String} access_token 인증키
- * @apiParam (AdminUserL_Action_2) {String} search_id 검색어(userid)
- * @apiParam (AdminUserL_Action_2) {String} search_name 검색어(username)
- * @apiParam (AdminUserL_Action_2) {String} page 검색 결과 페이지
+ * @apiParam {String} access_token 인증키
+ * @apiParam {String} [page] 페이지
  *
  * @apiSuccess {String} result "success" or "fail"
  * @apiSuccess {String} err_msg result가 "fail"인 경우
- * @apiSuccess {String} data result가 "success"인 경우
- * 
+ * @apiSuccess {Object[]} data 멤버 정보 목록
+ * @apiSuccess {String} data.userno 사용자 no
+ * @apiSuccess {String} data.userid login id(email)
+ * @apiSuccess {String} data.usertype 5:사용자, 9:관리자
+ * @apiSuccess {String} data.dispname 대화명
+ * @apiSuccess {String} data.intro 자기소개
+ * @apiSuccess {String} data.profile 프로필 사진
+ * @apiSuccess {String} data.create_datetime 가입일시
+ * @apiSuccess {String} data.phone_number 전화번호
+ * @apiSuccess {String} data.state 1:ON(사용), 0:OFF(탈퇴)
+ * @apiSuccess {String} data.activated 2:관리자 인증, 1:URL 인증,0:OFF 미인증
+ * @apiSuccess {String} data.public 1 = 공개 0 = 비공개
  */
+
 /**
- * @apiDefine AdminUserL_Action_1 Parameter(SubAction 1)
- */
-/**
- * @apiDefine AdminUserL_Action_2 Parameter(SubAction 2)
+ * @api {post} /api/admin/userS userS
+ * @apiVersion 0.1.0
+ * @apiName AdminUserS
+ * @apiDescription 가계부 관리자 회원 검색
+ * @apiGroup Admin_User
+ *
+ * @apiExample {curl} Example :
+ * curl -X POST  -H "Accept: Application/json" -H "Content-Type: application/json" \
+-d '{"action":"1", "access_token":"access_token_value"}' \
+http://localhost:8082/api/admin/userL
+ *
+ * @apiParam {String} access_token 인증키
+ * @apiParam {String} [page] 페이지
+ * @apiParam {Object[]} search 검색 필터
+ * @apiParam {String} [search.userid] login id(email)
+ * @apiParam {String} [search.usertype] 5:사용자, 9:관리자
+ * @apiParam {String} [search.dispname] 대화명
+ * @apiParam {String} [search.intro] 자기소개
+ * @apiParam {String} [search.date_begin] 가입 시작 일시
+ * @apiParam {String} [search.date_end] 가입 종료 일시
+ * @apiParam {String} [search.phone_number] 전화번호
+ * @apiParam {String} [search.state] 1:ON(사용), 0:OFF(탈퇴)
+ * @apiParam {String} [search.activated] 2:관리자 인증, 1:URL 인증,0:OFF 미인증
+ *
+ * @apiSuccess {String} result "success" or "fail"
+ * @apiSuccess {String} err_msg result가 "fail"인 경우
+ * @apiSuccess {Object[]} data 멤버 정보 목록
+ * @apiSuccess {String} data.userno 사용자 no
+ * @apiSuccess {String} data.userid login id(email)
+ * @apiSuccess {String} data.usertype 5:사용자, 9:관리자
+ * @apiSuccess {String} data.dispname 대화명
+ * @apiSuccess {String} data.intro 자기소개
+ * @apiSuccess {String} data.profile 프로필 사진
+ * @apiSuccess {String} data.create_datetime 가입일시
+ * @apiSuccess {String} data.phone_number 전화번호
+ * @apiSuccess {String} data.state 1:ON(사용), 0:OFF(탈퇴)
+ * @apiSuccess {String} data.activated 2:관리자 인증, 1:URL 인증,0:OFF 미인증
+ * @apiSuccess {String} data.public 1 = 공개 0 = 비공개
  */
