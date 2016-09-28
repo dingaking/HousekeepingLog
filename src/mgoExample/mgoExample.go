3fb6602d94cb8890443bb13fa52472ea5a8abac6
HWANGJUNG TEK <dingaking@gmail.com>
2016-09-29 08:42:03 +0900

    add&split sample

--- src/mgoExample/mgoExample.go
+++ src/mgoExample/mgoExample.go
@@ -25,11 +25,110 @@ var (
 )
 
 func main() {
+
+	make_database()
+	sample_insert()
+	sample_select_one()
+	sample_select_all()
+	sample_update()
+}
+
+func sample_update() {
+
 	session, err := mgo.Dial("localhost")
 	if err != nil {
 		panic(err)
 	}
+	defer session.Close()
 
+	// Collection People
+	c := session.DB("test").C("people")
+
+	// Update
+	colQuerier := bson.M{"name": "Ale"}
+	change := bson.M{"$set": bson.M{"phone": "+86 99 8888 7777", "timestamp": time.Now()}}
+	err = c.Update(colQuerier, change)
+	if err != nil {
+		panic(err)
+	}
+
+	// Query All
+	var results []Person
+	err = c.Find(bson.M{"name": "Ale"}).Sort("-timestamp").All(&results)
+
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Results All: ", results)
+}
+
+func sample_select_all() {
+
+	session, err := mgo.Dial("localhost")
+	if err != nil {
+		panic(err)
+	}
+	defer session.Close()
+
+	// Collection People
+	c := session.DB("test").C("people")
+
+	// Query All
+	var results []Person
+	err = c.Find(bson.M{"name": "Ale"}).Sort("-timestamp").All(&results)
+
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Results All: ", results)
+}
+
+func sample_select_one() {
+
+	session, err := mgo.Dial("localhost")
+	if err != nil {
+		panic(err)
+	}
+	defer session.Close()
+
+	// Collection People
+	c := session.DB("test").C("people")
+
+	// Query One
+	result := Person{}
+	err = c.Find(bson.M{"name": "Ale"}).Select(bson.M{"phone": 0}).One(&result)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Phone", result)
+}
+
+func sample_insert() {
+
+	session, err := mgo.Dial("localhost")
+	if err != nil {
+		panic(err)
+	}
+	defer session.Close()
+
+	// Collection People
+	c := session.DB("test").C("people")
+
+	// Insert Datas
+	err = c.Insert(&Person{Name: "Ale", Phone: "+55 53 1234 4321", Timestamp: time.Now()},
+		&Person{Name: "Cla", Phone: "+66 33 1234 5678", Timestamp: time.Now()})
+
+	if err != nil {
+		panic(err)
+	}
+}
+
+func make_database() {
+
+	session, err := mgo.Dial("localhost")
+	if err != nil {
+		panic(err)
+	}
 	defer session.Close()
 
 	session.SetMode(mgo.Monotonic, true)
@@ -58,45 +157,4 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-
-	// Insert Datas
-	err = c.Insert(&Person{Name: "Ale", Phone: "+55 53 1234 4321", Timestamp: time.Now()},
-		&Person{Name: "Cla", Phone: "+66 33 1234 5678", Timestamp: time.Now()})
-
-	if err != nil {
-		panic(err)
-	}
-
-	// Query One
-	result := Person{}
-	err = c.Find(bson.M{"name": "Ale"}).Select(bson.M{"phone": 0}).One(&result)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println("Phone", result)
-
-	// Query All
-	var results []Person
-	err = c.Find(bson.M{"name": "Ale"}).Sort("-timestamp").All(&results)
-
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println("Results All: ", results)
-
-	// Update
-	colQuerier := bson.M{"name": "Ale"}
-	change := bson.M{"$set": bson.M{"phone": "+86 99 8888 7777", "timestamp": time.Now()}}
-	err = c.Update(colQuerier, change)
-	if err != nil {
-		panic(err)
-	}
-
-	// Query All
-	err = c.Find(bson.M{"name": "Ale"}).Sort("-timestamp").All(&results)
-
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println("Results All: ", results)
 }
