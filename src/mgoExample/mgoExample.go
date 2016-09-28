81a43ca91403172e4cc2a6a7f138f3ac8db05632
HWANGJUNG TEK <dingaking@gmail.com>
2016-09-29 08:49:53 +0900

    delete sample.

--- src/mgoExample/mgoExample.go
+++ src/mgoExample/mgoExample.go
@@ -31,6 +31,34 @@ func main() {
 	sample_select_one()
 	sample_select_all()
 	sample_update()
+	sample_delete()
+}
+
+func sample_delete() {
+
+	session, err := mgo.Dial("localhost")
+	if err != nil {
+		panic(err)
+	}
+	defer session.Close()
+
+	// Collection People
+	c := session.DB("test").C("people")
+
+	// Delete(Remove)
+	err = c.Remove(bson.M{"name": "Ale"})
+	if err != nil {
+		panic(err)
+	}
+
+	// Query All
+	var results []Person
+	err = c.Find(bson.M{"name": "Ale"}).Sort("-timestamp").All(&results)
+
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Results All: ", results)
 }
 
 func sample_update() {
