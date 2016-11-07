190c070ebae2e366e737b6e35c15619e3a92695d
dinga <dingaking@gmail.com>
2016-11-07 23:00:08 +0900

    remove lint error

--- src/apis/query/AdminCommon.go
+++ src/apis/query/AdminCommon.go
@@ -8,18 +8,19 @@ import (
 	"labix.org/v2/mgo/bson"
 )
 
-func CheckPermission(s *mgo.Session, access_token string) error {
+// CheckPermission : check token is admin.
+func CheckPermission(s *mgo.Session, accessToken string) error {
 
 	c := s.DB(DatabaseName).C(CollUser)
 
 	var result model.User
-	c.Find(bson.M{"access_token": access_token}).One(&result)
+	c.Find(bson.M{"access_token": accessToken}).One(&result)
 
 	if result.UserNo == "" {
-		return errors.New("wrong access token error.")
+		return errors.New("wrong access token error")
 	}
 	if result.UserType != 4 {
-		return errors.New("not admin error.")
+		return errors.New("not admin error")
 	}
 
 	return nil
